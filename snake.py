29b7df4a83c4cf1487652af1dda93d6b403b7096
Colin <46822888+ColinLeDev@users.noreply.github.com>
2021-04-01 16:56:07 +0200

    Check la partie suivante :
    	# si la serpent arrive au bord de la fenêtre (20 lignes x 60 colonnes)
    	if snake[0][0] == 0:
    		snake[0][0] = win.getmaxyx()[0]-2
    
    	if snake[0][1] == 0:
    		snake[0][1] = win.getmaxyx()[1]-2
    
    	if snake[0][0] == win.getmaxyx()[0]-1:
    	  snake[0][0] = 1
    
    	if snake[0][1] == win.getmaxyx()[1]-1:
    	  snake[0][1] = 1
    
    
    	# Suppression du dernier anneau du serpent.
    	# Sera conditionner plus tard au fait que le serpent mange ou pas une pomme
    	# Le score sera alors également mis à jour
    	food, snake, last, score = mange_pomme(win, food, snake, score)
    
    
    	# Affichage de la tête à sa nouvelle position en bleu sur fond jaune
    	win.addstr(snake[0][0], snake[0][1], '*', curses.color_pair(3))
    
    	# Effacement du dernier anneau : affichage du caractère "espace" sur fond noir
    	win.addstr(last[0], last[1], ' ', curses.color_pair(1))
    
    	# Affichage du score dans l'aire de jeu
    	win.addstr(0, 2, 'Score : ' + str(score) + ' ')
    
    	# Attendre avant le pas suivant
    	vitesse = plus_vite(score)
    	win.timeout(150//vitesse)
    
    	# tuple contenant :
    	# - la liste des positions en cours des anneaux du serpent
    	# - score en cours
    	return snake, score

--- snake.py
+++ snake.py
@@ -1,5 +1,7 @@
 import time as tps
 import curses
+from curses import binascii as ba
+from curses import KEY_RIGHT, KEY_LEFT, KEY_UP, KEY_DOWN
 titolo="PythonSnake"
 titre =['  _______     _________ _    _  ____  _   _    _____ _   _          _  ________ ',
         ' |  __ \ \   / |__   __| |  | |/ __ \| \ | |  / ____| \ | |   /\   | |/ |  ____|',
@@ -42,7 +44,7 @@ def affichage_aire_de_jeu(hauteur, largeur, tit):
     # retourner la fenêtre
     return win
 
-def control(win, key, keys = [____]):
+def control(win, key, keys = [KEY_RIGHT, KEY_LEFT, KEY_UP, KEY_DOWN, 27]):
 	'''
 	Controles de jeu
 	paramètres :
@@ -61,7 +63,7 @@ def control(win, key, keys = [____]):
 	# Si aucune touche actionnée (pas de nouveau caractère)
 	# ou pas dans la liste des touches acceptées
 	# key prend la valeur de la dernière touche connue
-	if key == None or key not in keys :
+	if key == "" or key not in keys :
 		key = old_key
 
 	# Raffaichissement de la fenètre
@@ -71,9 +73,87 @@ def control(win, key, keys = [____]):
 	return key
 
 
+def jeu(win):
+	'''
+	Moteur du jeu
+	paramètre :
+	  win : fenètre en cours
+	retour :
+	  score à la fin du jeu
+	'''
+
+	# initialisation du jeu
+	# Le serpent se dirige vers la droite au début du jeu.
+	# C'est comme si le joueur avait utilisé la flèche droite au clavier
+	key = KEY_RIGHT
+	score = 0
+
+	# Definition des coordonnées du serpent
+	# Le serpent est une liste de d'anneaux composées de leurs coordonnées ligne, colonne
+	# La tête du serpent est en 4,10, l'anneau 1 en 4,9, le 2 en 4,8
+	snake = [[4, 10], [4, 9], [4, 8]]
+
+	# La nouriture (pomme) se trouve en 10,20
+	food = [10, 20]
+
+	# Affichage la nouriture en vert sur fond noir dans la fenêtre
+	curses.init_pair(2, curses.green, curses.black)
+	win.addch(food[0], food[1], chr(211), curses.color_pair(2))  # Prints the food
+
+	# Affichage du serpent en bleu sur fond jaune
+	curses.init_pair(3, curses.blue, curses.yellow)
+	# sur toute la longeur du serpent
+	for i in range(1,3):
+		# affichage de chaque anneau dans la fenêtre en ligne, colonne
+		win.addstr(snake[i][0], snake[i][1], '*', curses.color_pair(3))
+
+	# Emission d'un beep  au début du jeu
+	curses.beep()
+
+	# Tant que le joueur n'a pas quitter le jeu
+	end = False
+	while key != 27 and not end:
+		key = controle(win, key)
+		snake, score = deplacement(win, score, key, snake, food)
+		end = perdu(win, snake)
+	return score
 
 
 
+
+
+
+def deplacement(win, score, key, snake, food):
+	'''
+	Déplacements du serpent
+	paramètres :
+	  win : fenètre en cours
+	  score : score en cours
+	  key : touche de controle en cours
+	  snake : liste des positions en cours des anneaux du serpent
+	  food : liste de la position de la pomme
+	retourne :
+	  tuple contenant la liste des positions en cours des anneaux du serpent et score en cours
+	'''
+	# Si on appui sur la flèche "à droite",
+	# la tête se déplace de 1 caractère vers la droite (colonne + 1)
+	if key == KEY_RIGHT:
+		snake.insert(0, [snake[0][0], snake[0][1]+1])
+
+	# Sinon si on appui sur la flèche "à gauche",
+	# la tête se déplace de 1 caractère vers la gauche (colonne - 1)
+	elif key == KEY_LEFT:
+		snake.insert(0, [snake[0][0], snake[0][1]-1])
+
+	# Sinon si on appui sur la flèche "en haut",
+	# la tête se déplace de 1 caractère vers le haut (ligne - 1)
+	elif key == KEY_UP:
+		snake.insert(0, [snake[0][0]-1, snake[0][1]])
+
+	# Sinon si on appui sur la flèche "en bas",
+	# la tête se déplace de 1 caractère vers le bas (ligne + 1)
+	elif key == KEY_DOWN:
+		snake.insert(0, [snake[0][0]+1, snake[0][1]])
 
 
 
